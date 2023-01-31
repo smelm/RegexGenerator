f9167dd1e9fa176478b3a5cccdd0d2a82f4ba3b1
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2023-01-31 14:26:39 +0100

    prepare demo

---
+++ MaleRegexTree/src/main/java/it/units/inginf/male/tree/operator/MatchOneOrMoreGreedy.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 Machine Learning Lab - University of Trieste, 
+ * Italy (http://machinelearning.inginf.units.it/)  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.units.inginf.male.tree.operator;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import it.units.inginf.male.tree.DescriptionContext;
+
+/**
+ *
+ * @author MaleLabTs
+ */
+public class MatchOneOrMoreGreedy extends Quantifier {
+
+    @Override
+    public void describe(StringBuilder builder, DescriptionContext context, RegexFlavour flavour) {
+        getChildrens().get(0).describe(builder, context, flavour);
+        builder.append("+");
+    }
+
+    @Override
+    protected UnaryOperator buildCopy() {
+        return new MatchOneOrMoreGreedy();
+    }
+
+    @Override
+    public JsonObject toJson() {
+        var obj = new JsonObject();
+        obj.addProperty("type", "repeat");
+        obj.add("lower", new JsonPrimitive(1));
+        obj.add("upper", null);
+        obj.add("child", getChildrens().get(0).toJson());
+        obj.addProperty("lazy", false);
+
+        return obj;
+    }
+}
---
+++ MaleRegexTree/src/main/java/it/units/inginf/male/tree/operator/MatchZeroOrMore.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2015 Machine Learning Lab - University of Trieste, 
+ * Italy (http://machinelearning.inginf.units.it/)  
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.units.inginf.male.tree.operator;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import it.units.inginf.male.tree.DescriptionContext;
+import it.units.inginf.male.tree.Node;
+
+/**
+ *
+ * @author MaleLabTs
+ */
+public class MatchZeroOrMore extends Quantifier {
+
+    @Override
+    public void describe(StringBuilder builder, DescriptionContext context, RegexFlavour flavour) {
+        StringBuilder tmp = new StringBuilder();
+        Node child = getChildrens().get(0);
+        // Counts the group immediatly
+        int index = context.incGroups();
+        child.describe(tmp, context, flavour);
+        int l = child.isEscaped() ? tmp.length() - 1 : tmp.length();
+        boolean group = l > 1 && !child.isCharacterClass() && !(child instanceof Group) && !(child instanceof NonCapturingGroup);
+        switch (flavour) {
+            case JAVA:
+                if (group) {
+                    builder.append("(?:");
+                    builder.append(tmp);
+                    builder.append(")");
+                } else {
+                    builder.append(tmp);
+                }
+                builder.append("*+");
+                break;
+            default:
+                builder.append("(?=(");
+                if (group) {
+                    builder.append("(?:");
+                    builder.append(tmp);
+                    builder.append(")");
+                } else {
+                    builder.append(tmp);
+                }
+                builder.append("*))\\").append(index);
+                context.incExpansionGroups();
+             
+        }
+    }
+
+    @Override
+    protected UnaryOperator buildCopy() {
+        return new MatchZeroOrMore();
+    }
+
+    @Override
+    public JsonObject toJson() {
+        var obj = new JsonObject();
+        obj.addProperty("type", "repeat");
+        obj.add("lower", new JsonPrimitive(0));
+        obj.add("upper", null);
+        obj.add("child", getChildrens().get(0).toJson());
+        obj.addProperty("lazy", true);
+        return obj;
+    }
+}
